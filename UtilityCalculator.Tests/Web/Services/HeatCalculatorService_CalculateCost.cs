a16b25a3a77aae569ef0de744b3e7a499f05818b
Steve Smith <steve@kentsmiths.com>
2018-08-31 16:50:30 -0400

    Added tests

---
+++ UtilityCalculator.Tests/Web/Services/HeatCalculatorService_CalculateCost.cs
@@ -0,0 +1,21 @@
+using System;
+using UtilityCalculator.Web.Services;
+using Xunit;
+
+namespace UtilityCalculator.Tests
+{
+    public class HeatCalculatorService_CalculateCost
+    {
+        [Theory]
+        [InlineData(1500, "M", 52.74)]
+        [InlineData(2500, "M", 87.9)]
+        [InlineData(2500, "D", 175.8)]
+        public void ReturnsXGiven1500AndMild(int size, string climate, decimal expectedResult)
+        {
+            var result = HeatCalculatorService.CalculateCost(size, climate);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
