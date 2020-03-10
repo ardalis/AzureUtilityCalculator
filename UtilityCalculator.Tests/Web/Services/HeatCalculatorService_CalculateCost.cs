8ca2063a185dceda99bdb07c943f1905c9d24079
Steve Smith <steve@kentsmiths.com>
2020-03-09 20:02:58 -0400

    Updating for new demos

--- UtilityCalculator.Tests/Web/Services/HeatCalculatorService_CalculateCost.cs
+++ UtilityCalculator.Tests/Web/Services/HeatCalculatorService_CalculateCost.cs
@@ -6,16 +6,10 @@ namespace UtilityCalculator.Tests
 {
     public class HeatCalculatorService_CalculateCost
     {
-        [Theory]
-        [InlineData(1500, "M", 52.74)]
-        [InlineData(2500, "M", 87.9)]
-        [InlineData(2500, "D", 175.8)]
-        public void ReturnsXGiven1500AndMild(int size, string climate, decimal expectedResult)
+        [Fact]
+        public void ReturnsXGiven1500AndMild()
         {
-            var result = HeatCalculatorService.CalculateCost(size, climate);
 
-            Assert.Equal(expectedResult, result);
         }
-
     }
 }
